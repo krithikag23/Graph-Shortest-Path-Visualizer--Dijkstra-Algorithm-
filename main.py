0f38d0baffa2d320f3ddaf5482adffac89d9c14b
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-09 12:45:46 +0530

    update

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ with open("graph_data.json") as f:
     graph = json.load(f)
 
 start = input("Enter start node: ").strip()
-target = input("Enter target node: ").strip()    
+target = input("Enter target node: ").strip()
+
+if start not in graph or target not in graph:
+    print("Invalid nodes. Try again.")
+    exit()
