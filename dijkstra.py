c6529a766030c9e79ae8fccf21f20cb61e31f351
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-13 21:25:47 +0530

    update

--- dijkstra.py
+++ dijkstra.py
@@ -5,14 +5,18 @@ import heapq
 def dijkstra(graph, start):
     dist = {node: float("inf") for node in graph}
     dist[start] = 0
-    pq = [(0, start)]  # (distance, node)
     parent = {node: None for node in graph}
+
+    pq = [(0, start)]
+    visited_count = 0
 
     while pq:
         curr_dist, node = heapq.heappop(pq)
 
         if curr_dist > dist[node]:
             continue
+
+        visited_count += 1
 
         for neighbor, weight in graph[node]:
             new_dist = curr_dist + weight
@@ -21,11 +25,12 @@ def dijkstra(graph, start):
                 parent[neighbor] = node
                 heapq.heappush(pq, (new_dist, neighbor))
 
-    return dist, parent
+    return dist, parent, visited_count
+
 
 def get_path(parent, target):
     path = []
-    while target is not None:
+    while target:
         path.append(target)
         target = parent[target]
-    return list(reversed(path))
+    return path[::-1]
