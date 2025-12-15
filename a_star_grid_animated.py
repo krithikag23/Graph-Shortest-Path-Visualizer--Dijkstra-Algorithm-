5480fd4d829437c4b67525499627f079cfbc441a
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-15 12:32:39 +0530

    update

--- a_star_grid_animated.py
+++ a_star_grid_animated.py
@@ -4,3 +4,8 @@ import heapq
 
 def heuristic(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def get_neighbors(pos, grid):
+    moves = [(0,1),(1,0),(0,-1),(-1,0)]
+    rows, cols = len(grid), len(grid[0])
+    neighbors = []
