1f6db3cab3e2f121f547f84d7bf3caf6466e2a81
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-15 12:35:09 +0530

    update

--- grid_animate.py
+++ grid_animate.py
@@ -2,3 +2,9 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
+from grid import GRID, START, GOAL
+from a_star_grid_animated import a_star_animated, reconstruct_path
+
+parent, visited = a_star_animated(GRID, START, GOAL)
+path = reconstruct_path(parent, GOAL)
