7c95ac4e29809e76140703259c039f3fe59feb04
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-15 12:35:21 +0530

    update

--- grid_animate.py
+++ grid_animate.py
@@ -8,3 +8,6 @@ from a_star_grid_animated import a_star_animated, reconstruct_path
 
 parent, visited = a_star_animated(GRID, START, GOAL)
 path = reconstruct_path(parent, GOAL)
+
+rows, cols = len(GRID), len(GRID[0])
+fig, ax = plt.subplots()
