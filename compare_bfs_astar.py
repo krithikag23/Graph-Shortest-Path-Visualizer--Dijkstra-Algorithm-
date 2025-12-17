05d76787d105c7b30e6d5706de8d95361864ce42
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-17 20:53:14 +0530

    update

--- compare_bfs_astar.py
+++ compare_bfs_astar.py
@@ -4,3 +4,4 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from grid import GRID, START, GOAL
 from bfs_grid_animated import bfs_animated, reconstruct_path as bfs_path
+from a_star_grid_animated import a_star_animated, reconstruct_path as astar_path
