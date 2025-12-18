2fbfac6d36a1fd2f701938e83075775818b2b673
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-18 13:20:45 +0530

    update

--- compare_bfs_astar.py
+++ compare_bfs_astar.py
@@ -5,3 +5,6 @@ from matplotlib.animation import FuncAnimation
 from grid import GRID, START, GOAL
 from bfs_grid_animated import bfs_animated, reconstruct_path as bfs_path
 from a_star_grid_animated import a_star_animated, reconstruct_path as astar_path
+
+# Run BFS and A*
+bfs_parent, bfs_visited = bfs_animated(GRID, START, GOAL)
